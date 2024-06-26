85ac3cf2a7186d48aca34a1aba8120dc24f65002
unknown <gibogithub>
2024-06-27 06:54:14 +0800

    second commit

--- facebook-loginpage/src/App.jsx
+++ facebook-loginpage/src/App.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import "./App.css";
-import LoginPage from "./Login/LoginPage";
 
 export default function App() {
-  return (
-    <div className="App">
-      <LoginPage />
-    </div>
-  );
+  return <div className="App"></div>;
 }
